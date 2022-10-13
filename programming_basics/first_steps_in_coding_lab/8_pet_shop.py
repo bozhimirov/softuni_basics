f60d36689a3fd18bc14cdd7efe3c273cf08c1fc2
Stanislav Bozhimirov <bozhimirov@gmail.com>
2022-10-14 00:23:26 +0300

    ok

---
+++ programming_basics/first_steps_in_coding_lab/8_pet_shop.py
@@ -0,0 +1,7 @@
+number_dog_packages = int(input())
+number_cat_packages = int(input())
+price_dog_food = number_dog_packages * 2.5
+price_cat_food = number_cat_packages * 4
+total = price_cat_food + price_dog_food
+
+print(f'{total} lv.')
