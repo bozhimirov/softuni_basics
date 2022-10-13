e3106cfa026b7d175e42a875c43d5d38c7fdcdf6
Stanislav Bozhimirov <bozhimirov@gmail.com>
2022-10-13 11:02:28 +0300

    ok

---
+++ programming_basics/first_steps_in_coding_lab/7_project_creation.py
@@ -0,0 +1,6 @@
+name_of_architect = input()
+number_of_projects = int(input())
+time_for_all_projects = number_of_projects * 3
+
+print(f'The architect {name_of_architect} will '
+      f'need {time_for_all_projects} hours to complete {number_of_projects} project/s.')
