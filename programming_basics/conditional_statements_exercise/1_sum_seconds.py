4268a652187c7fd593eaf723d1e56f1efdb0f2ab
Stanislav Bozhimirov <bozhimirov@gmail.com>
2022-10-20 20:04:34 +0300

    ok

---
+++ programming_basics/conditional_statements_exercise/1_sum_seconds.py
@@ -0,0 +1,12 @@
+import math
+time_first = int(input())
+time_second = int(input())
+time_third = int(input())
+total_time = time_third + time_second + time_first
+minutes = total_time // 60
+seconds = total_time % 60
+minutes = math.floor(minutes)
+if seconds < 10:
+    print(f'{minutes}:0{seconds}')
+else:
+    print(f"{minutes}:{seconds}")
