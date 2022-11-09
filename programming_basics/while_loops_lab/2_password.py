ad0d673971e20f3cbabd159d81bc22b00f0c5fbf
Stanislav Bozhimirov <bozhimirov@gmail.com>
2022-11-09 12:41:06 +0200

    tasks

---
+++ programming_basics/while_loops_lab/2_password.py
@@ -0,0 +1,7 @@
+username = input()
+password = input()
+entered_password = input()
+while entered_password != password:
+    entered_password = input()
+else:
+    print(f"Welcome {username}!")
---
+++ programming_basics/while_loops_lab/3_sum_numbers.py
@@ -0,0 +1,7 @@
+start_number = int(input())
+sum = 0
+while sum < start_number:
+    number = int(input())
+    sum += number
+else:
+    print(sum)
